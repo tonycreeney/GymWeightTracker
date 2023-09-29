99e82ce6e5c4783f435bbb380cc04b285250b2b1
Tony Creeney <tonycreeney@gmail.com>
2023-09-29 02:28:12 +0100

    First Commit of Gym Weight Tracker

---
+++ gymweighttracker.js
@@ -0,0 +1,5 @@
+//Create an interface that will ask me to choose from a set of options--Calender, current high scores, and add a new entry.
+//Maybe add another option that is quick add scores - get to choose what day to add it to (I'll need to set up a database here)
+//This could work better as a website, so i can use html and css to design the website, then add a database and the interactivity using javascript
+//If i create a webpage, I can have multiple tabs
+//Then I can experient with adding it to a server. this will help me gain knowledge on that side of the coin too.
